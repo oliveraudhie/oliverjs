1fbf116cdb4697814f2d9e3c5e65160d8eafb752
oliveraudhie <slumantri@gmail.com>
2018-01-12 18:54:33 +0700

    prompt baterai, kondisi, derajat

--- tes.js
+++ tes.js
@@ -1,16 +1,25 @@
 let baterai = prompt('Berapa persen sisa baterai HP anda?')
-if (baterai > 50) {
+if (baterai > 50 && baterai < 100) {
   console.log('Nanti saja charge HP')
 } else {
   console.log('Siapkan charger anda')
 }
 
 let kondisiTubuh = prompt('Apakah anda capek? (y/t/?)')
-if (kondisiTubuh == 'y', 'Y') {
+if (kondisiTubuh === 'y' || kondisiTubuh === 'Y') {
   console.log('Break Time')
 } else
-if (kondisiTubuh == 't', 'T') {
+if (kondisiTubuh === 't' || kondisiTubuh === 'T') {
   console.log('Yakin?')
 } else {
   console.log('Anda Ragu?')
 }
+
+const celcius = prompt('berapa suhu celcius?')
+const kelvin = +celcius + 273.15
+const fahrenheit = +(celcius * 1.8) + 32
+console.log(
+  kelvin + ' Kelvin'
+)
+document.getElementById("hasil").innerHTML = fahrenheit + ' Fahrenheit'
+console.log(fahrenheit + ' Fahrenheit')
