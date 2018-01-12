8c35d94cc68d47c8ca7b21a5e6a421c62724b9af
M Haidar Hanif <git@mhaidarhanif.com>
2018-01-12 19:02:50 +0700

    Fix some JS syntaxes

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ let jawaban = prompt('Berapakah 1 + 2?')
 
 if (jawaban == 3) {
   console.log('100 Untuk Anda')
-}
-// gimana biar bisa jalan?
-// else if (jawaban === 5, 4, 2, 1, 0) {
-// console.log('Hampir Benar!')
+} else if (jawaban === 5) {
+  console.log('Hampir Benar!')
 } else {
   console.log('Gunakan Kalkulator!')
 }
